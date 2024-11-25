a06682a62229c2d8d6c75d6e90342c29e2708419
BusuiocDamian <busuioc.damian@elev.cihcahul.md>
2024-11-25 12:15:10 +0200

    Am adaugat bara de meniuri

--- script.js
+++ script.js
@@ -3,12 +3,26 @@ const panels = document.querySelectorAll(".panel");
 panels.forEach(panel => {
   panel.addEventListener("click", () => {
     removeActiveClasses();
-    panel.classList.add("active");   
+    panel.classList.add("active");
   });
 });
 
 function removeActiveClasses() {
-    panels.forEach(panel => {
-      panel.classList.remove("active");
-    });
+  panels.forEach(panel => {
+    panel.classList.remove("active");
+  });
 }
+
+document.querySelectorAll('.menu a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const targetId = link.getAttribute('href').substring(1);
+    const targetPanel = document.getElementById(targetId);
+
+    if (targetPanel) {
+      removeActiveClasses();
+      targetPanel.classList.add('active');
+    }
+  });
+});
+
